8ae91578885e46822764045529595ac86af5fc2b
walter <w.kocher01@ufromail.cl>
2022-09-30 17:24:09 -0300

    curso

--- src/intranet/Curso.java
+++ src/intranet/Curso.java
@@ -6,7 +6,7 @@ public class Curso {
         this.alumnos = new ArrayList<Alumno>();
     }
     public Alumno agregarAlumno(String nombre,String apellido, String rut, String matricula){
-        Alumno alumno = new Alumno(nombre,apellido,rut,apellido);
+        Alumno alumno = new Alumno(nombre,apellido,rut,matricula);
         this.alumnos.add(alumno);
         return alumno;
     }
@@ -14,13 +14,47 @@ public class Curso {
         this.alumnos.remove(alumno);
 
     }
-    public boolean removerAlumno(String nombre, String rut){
+    public boolean removerAlumno(String nombre, String apellido, String rut, String matricula){
         for(Alumno alumno: this.alumnos){
-            if (alumno.getnombre().equals(nombre) && alumno.getrut().equals(rut)){
+            if (alumno.getnombre().equals(nombre) && alumno.getapellidos().equals(apellido) && alumno.getrut().equals(rut) && alumno.getmatricula().equals(matricula)){
                 this.alumnos.remove(alumno);
                 return true;
             }
         }
         return false;
     }
+
+    public void modificarRutAlumno(String nombre, String apellido,String matricula, String rut){
+        for(Alumno alumno: this.alumnos){
+            if(alumno.getnombre().equals(nombre) && alumno.getapellidos().equals(apellido) && alumno.getmatricula().equals(matricula)){
+                alumno.setRut(rut);
+                break;
+            }
+        }
+    }
+    public void modificarMatriculaAlumno(String nombre, String apellido, String rut,String matricula){
+        for(Alumno alumno: this.alumnos){
+            if(alumno.getnombre().equals(nombre) && alumno.getapellidos().equals(apellido) && alumno.getrut().equals(rut)){
+                alumno.setmatricula(matricula);
+                break;
+            }
+        }
+    }
+    public void modificarNombreAlumno(String nombre, String apellido, String rut,String matricula){
+        for(Alumno alumno: this.alumnos){
+            if(alumno.getapellidos().equals(apellido) && alumno.getrut().equals(rut) && alumno.getmatricula().equals(matricula)){
+                alumno.setnombre(nombre);
+                break;
+            }
+        }
+    }
+    public void modificarApellidoAlumno(String nombre, String apellido, String rut,String matricula){
+        for(Alumno alumno: this.alumnos){
+            if(alumno.getnombre().equals(nombre) && alumno.getrut().equals(rut) && alumno.getmatricula().equals(matricula)){
+                alumno.setapellidos(apellido);
+                break;
+            }
+        }
+    }
+
 }
